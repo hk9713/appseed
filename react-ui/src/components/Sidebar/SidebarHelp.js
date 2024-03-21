5285c0a727eafd7656449b7ae5c513004b539854
hk9713 <96482380+hk9713@users.noreply.github.com>
2024-03-21 15:09:50 +0900

    Sidebar - Title 변경

--- react-ui/src/components/Sidebar/SidebarHelp.js
+++ react-ui/src/components/Sidebar/SidebarHelp.js
@@ -4,6 +4,7 @@ import SidebarHelpImage from "assets/img/SidebarHelpImage.png";
 import IconBox from "components/Icons/IconBox";
 import React from "react";
 
+
 export function SidebarHelp(props) {
   // Pass the computed styles into the `__css` prop
   const { children, ...rest } = props;
